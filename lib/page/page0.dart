f9184610e0b39058920f0e80bdc14ec9c921607e
Obaydullah <obaydullahkhaan@gmail.com>
2023-10-02 13:19:54 +0600

    page0 added

--- lib/page/page0.dart
+++ lib/page/page0.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'home.dart';
+
 class Page0 extends StatelessWidget {
   const Page0({super.key});
 
@@ -7,9 +8,54 @@ class Page0 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-      //  color: Colors.redAccent,
-      //  color: Colors.redAccent,
-        child: SvgPicture.asset("asset/chairx.svg",fit: BoxFit.cover,),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('asset/bg.png'), fit: BoxFit.cover),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 49.0,
+            bottom: 25,
+          ),
+          child: Column(
+            children: [
+              Text(
+                "We design \nFurniture for \nYour comfort",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+              InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => Home()));
+                },
+                child: Container(
+                  height: 99,
+                  width: 363,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.black,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
